8b43d85548de896f53158bece18ca78754d80fc1
borisovkaloyan <borisovkaloyan@gmail.com>
2023-03-29 19:22:23 +0300

    Add Ex3 to project

---
+++ WelcomeExtended/Helpers/UserHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Welcome.Model;
+using WelcomeExtended.Data;
+
+namespace WelcomeExtended.Helpers
+{
+    public static class UserHelper
+    {
+        public static void ToString(this User user)
+        {
+            Console.WriteLine("Found User with username: " + user.Name);
+            Console.WriteLine("Identifer: " + user.Id);
+            Console.WriteLine("Role: " + user.Role);
+        }
+
+        public static bool ValidateCredentials(UserData userData, string name, string password)
+        {
+            if (name == "")
+            {
+                throw new Exception("Name cannot be empty");
+            }
+
+            if (password == "")
+            {
+                throw new Exception("Password cannot be empty");
+            }
+
+            return userData.ValidateUser(name, password);
+        }
+
+        public static User GetUser(UserData userData, string name, string password)
+        {
+            if (name == "")
+            {
+                throw new Exception("Name cannot be empty");
+            }
+
+            if (password == "")
+            {
+                throw new Exception("Password cannot be empty");
+            }
+
+            return userData.GetUser(name, password);
+        }
+    }
+}
--- WelcomeExtended/Program.cs
+++ WelcomeExtended/Program.cs
@@ -6,6 +6,9 @@ using Welcome.ViewModel;
 using Welcome.Model;
 using static WelcomeExtended.Others.Delegates;
 using WelcomeExtended.Loggers;
+using WelcomeExtended.Data;
+using WelcomeExtended.Helpers;
+using System.Xml.Linq;
 
 namespace WelcomeExtended
 {
@@ -15,31 +18,63 @@ namespace WelcomeExtended
         {
             try
             {
-                var user = new User
+                UserData userData = new UserData();
+
+                User studentUser = new User()
                 {
-                    Name = "John Smith",
-                    Password = "password123",
+                    Name = "Student",
+                    Password = "123",
                     Role = UserRolesEnum.STUDENT
                 };
 
-                var viewModel = new UserViewModel(user);
-                var view = new UserView(viewModel);
+                userData.AddUser(studentUser);
 
-                view.DisplayBasic();
+                User studentUser2 = new User()
+                {
+                    Name = "Student2",
+                    Password = "123",
+                    Role = UserRolesEnum.STUDENT
+                };
 
-                view.DisplayError();
+                userData.AddUser(studentUser2);
+
+                User teacherUser = new User()
+                {
+                    Name = "Teacher",
+                    Password = "1234",
+                    Role = UserRolesEnum.PROFESSOR
+                };
+
+                userData.AddUser(teacherUser);
+
+                User adminUser = new User()
+                {
+                    Name = "Admin",
+                    Password = "12345",
+                    Role = UserRolesEnum.ADMIN
+                };
+
+                userData.AddUser(adminUser);
+
+                string username;
+                Console.WriteLine("Input username: ");
+                username = Console.ReadLine();
+
+                string password;
+                Console.WriteLine("Input password: ");
+                password = Console.ReadLine();
+
+                UserHelper.ValidateCredentials(userData, username, password);
+
+                User gotUser = new User();
+                gotUser = UserHelper.GetUser(userData, username, password);
+
+                UserHelper.ToString(gotUser);
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
                 var log = new ActionOnError(Log);
-                log(e.Message);
-
-                log = new ActionOnError(logFile);
-                log(e.Message);
-            }
-            finally 
-            {
-                Console.WriteLine("Executed in any case!");
+                log(ex.Message);
             }
         }
     }
