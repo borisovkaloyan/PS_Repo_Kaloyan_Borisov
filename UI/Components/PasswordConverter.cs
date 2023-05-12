33b8ece23b76a98dd93870014a35badab3f5d677
Kaloyan Borisov <kaborisov@tu-sofia.bg>
2023-05-12 21:43:49 +0300

    Add ex5

---
+++ UI/Components/PasswordConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace UI.Components
+{
+    public class PasswordConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            char[] passChars = value.ToString().ToCharArray();
+
+            for(int i = 0; i < passChars.Length; i++)
+            {
+                passChars[i] += (Char)1;
+            }
+                
+            return new string(passChars);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
